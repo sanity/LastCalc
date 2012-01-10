798a95d6a1be0a058a0a0f5f1265c24a134bda44
Ian Clarke <ian.clarke@gmail.com>
2012-01-10 16:07:55 -0600

    currencies now work

--- src/com/lastcalc/Renderers.java
+++ src/com/lastcalc/Renderers.java
@@ -1,7 +1,10 @@
 package com.lastcalc;
 
+import java.text.*;
+
 import javax.measure.unit.Unit;
 
+import org.jscience.economics.money.Currency;
 import org.jscience.physics.amount.Amount;
 import org.jsoup.nodes.*;
 import org.jsoup.parser.Tag;
@@ -11,7 +14,7 @@ import com.lastcalc.parsers.amounts.UnitParser;
 
 
 public class Renderers {
-
+	private static Format currencyFormat = new DecimalFormat("#.####");
 
 	public static Element toHtml(final String baseUri, final TokenList tokens) {
 		final Element ret = new Element(Tag.valueOf("span"), baseUri);
@@ -20,17 +23,33 @@ public class Renderers {
 				final Amount<?> amount = (Amount<?>) obj;
 				final Element amountSpan = ret.appendElement("span");
 				final double estimatedValue = amount.getEstimatedValue();
-				final String numStr = Misc.numberFormat.format(estimatedValue);
-				amountSpan.appendElement("span").addClass("number").text(numStr);
-				amountSpan.appendText(" ");
-				if (!amount.getUnit().equals(Unit.ONE)) {
-					final Element unitSpan = amountSpan.appendElement("span").addClass("recognized");
-					final String verboseName = estimatedValue == 1.0 ? UnitParser.verboseNamesSing.get(amount.getUnit())
-							: UnitParser.verboseNamesPlur.get(amount.getUnit());
-					if (verboseName != null) {
-						unitSpan.text(verboseName);
+				if (amount.getUnit() instanceof Currency) {
+					final Element currencySpan = amountSpan.appendElement("span").addClass("currency");
+					final Currency currency = (Currency) amount.getUnit();
+					if (currency.getCode().equalsIgnoreCase("USD")) {
+						currencySpan.text("US$" + currencyFormat.format(estimatedValue));
+					} else if (currency.getCode().equalsIgnoreCase("GBP")) {
+						currencySpan.text("£" + currencyFormat.format(estimatedValue));
+					} else if (currency.getCode().equalsIgnoreCase("EUR")) {
+						currencySpan.text("Û" + currencyFormat.format(estimatedValue));
+					} else if (currency.getCode().equalsIgnoreCase("JPY")) {
+						currencySpan.text("´" + currencyFormat.format(estimatedValue));
 					} else {
-						unitSpan.text(amount.getUnit().toString());
+						currencySpan.text(currencyFormat.format(estimatedValue) + currency.getCode());
+					}
+				} else {
+					final String numStr = Misc.numberFormat.format(estimatedValue);
+					amountSpan.appendElement("span").addClass("number").text(numStr);
+					amountSpan.appendText(" ");
+					if (!amount.getUnit().equals(Unit.ONE)) {
+						final Element unitSpan = amountSpan.appendElement("span").addClass("recognized");
+						final String verboseName = estimatedValue == 1.0 ? UnitParser.verboseNamesSing.get(amount.getUnit())
+								: UnitParser.verboseNamesPlur.get(amount.getUnit());
+						if (verboseName != null) {
+							unitSpan.text(verboseName);
+						} else {
+							unitSpan.text(amount.getUnit().toString());
+						}
 					}
 				}
 			} else if (obj instanceof UserDefinedParser) {
