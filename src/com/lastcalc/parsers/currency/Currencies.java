f186a02260df78935ed48699c014dfb742b2eca5
Ian Clarke <ian.clarke@gmail.com>
2012-07-19 08:09:56 -0500

    fix currencies, openexchagerate API had changed

--- src/com/lastcalc/parsers/currency/Currencies.java
+++ src/com/lastcalc/parsers/currency/Currencies.java
@@ -2,15 +2,15 @@
  * LastCalc - The last calculator you'll ever need
  * Copyright (C) 2011, 2012 Uprizer Labs LLC
  * 
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU Affero General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR 
- * PURPOSE.  See the GNU Affero General Public License for more 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more
  * details.
  ******************************************************************************/
 package com.lastcalc.parsers.currency;
@@ -51,9 +51,9 @@ public class Currencies {
 				parsers.add(new UnitParser(e.getValue(), TokenList.createD(e.getKey().toLowerCase())));
 			}
 			parsers.add(new UnitParser(currenciesByCode.get("USD"), TokenList.createD("$")));
-			parsers.add(new UnitParser(currenciesByCode.get("EUR"), TokenList.createD("Û")));
-			parsers.add(new UnitParser(currenciesByCode.get("JPY"), TokenList.createD("´")));
-			parsers.add(new UnitParser(currenciesByCode.get("GBP"), TokenList.createD("£")));
+			parsers.add(new UnitParser(currenciesByCode.get("EUR"), TokenList.createD("ï¿½")));
+			parsers.add(new UnitParser(currenciesByCode.get("JPY"), TokenList.createD("ï¿½")));
+			parsers.add(new UnitParser(currenciesByCode.get("GBP"), TokenList.createD("ï¿½")));
 
 			parsers.add(new CurrencyReverser());
 		}
@@ -64,7 +64,7 @@ public class Currencies {
 	public static void updateExchangeRates() {
 		Currency.setReferenceCurrency(Currency.USD);
 		try {
-			final URL url = new URL("https://raw.github.com/currencybot/open-exchange-rates/master/latest.json");
+			final URL url = new URL("http://openexchangerates.org/api/latest.json?app_id=d1bd706a92774feca9c15d615ae85685");
 			final Rates rates = Misc.gson.fromJson(new InputStreamReader(url.openStream()), Rates.class);
 			if (!rates.base.equalsIgnoreCase("usd")) {
 				log.warning("We assume base rate of USD, but it is " + rates.base);
